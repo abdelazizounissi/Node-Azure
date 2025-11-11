0cc181eceb61f35bcae806c3c83cf53f3d505aba
Abdelaziz Ounissi <w.ounissiabdelaziz@gmail.com>
2025-11-11 10:59:00 +0100

    Update

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-res.send('<h1>Bienvenue sur mon site Node.js hébergé sur Azure </h1><p>Nom : Abdelaziz Ounissi</p>');
+res.send(
+    '<h1>Bienvenue sur mon site Node.js hébergé sur Azure </h1>' +
+    '<p>Nom : Abdelaziz Ounissi</p>' +
+    '<p>Testing auto deployment from github to Azure</p>' +
+    '<p>Testing updating...</p>'
+);
 });
 app.listen(port, () => {
 console.log(`Application en cours d\'exécution sur le port
